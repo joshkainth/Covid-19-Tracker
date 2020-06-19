5c709589bbe69fbc1cdb1b9982f2bb13b7af51b6
Gurlivleen Singh Kainth <joshgskainth@gmail.com>
2020-06-19 18:17:07 +0530

    Google Map Running

--- app/src/main/java/com/technotechworldz/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/technotechworldz/covid_19tracker/MainActivity.java
@@ -1,18 +1,33 @@
 package com.technotechworldz.covid_19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(20.5937, 78.9629))
+                .title("Marker"));
     }
 }
